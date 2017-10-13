9ffad5f506ebdf6ac73c91115d59395ef93b9552
Wesley <tiankai@shinetechchina.com>
2017-10-13 15:33:54 +0800

    add image icon

--- src/upload/admin/language/en-gb/extension/payment/ipgpay.php
+++ src/upload/admin/language/en-gb/extension/payment/ipgpay.php
@@ -4,6 +4,10 @@ $_['heading_title']                  = 'IPG Pay';
 
 // Text
 $_['text_extension']                 = 'Extensions';
+$_['text_success']                   = 'Success: You have modified Ipgpay account details!';
+$_['text_edit']                      = 'Edit Ipgpay Pay';
+$_['text_ipgpay']                    = '<a target="_BLANK" href="https://open.ipgpay.com"><img src="view/image/payment/ipgpay.png" alt="Ipgpay Pay Website" title="Ipgpay Pay Website" style="border: 1px solid #EEEEEE;" /></a>';
+$_['text_sandbox']                   = 'Sandbox';
 
 // Entry
 $_['entry_app_id']                   = 'App ID';
