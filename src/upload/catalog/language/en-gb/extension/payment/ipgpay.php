1251ab2500d394f4027e1a4ba1e1c1a88938d9c1
Wesley <tiankai@shinetechsoftware.com>
2017-10-31 13:05:51 +0800

    to research order refund&capture&void

--- src/upload/catalog/language/en-gb/extension/payment/ipgpay.php
+++ src/upload/catalog/language/en-gb/extension/payment/ipgpay.php
@@ -1,15 +1,1 @@
-<?php
-// Heading
-$_['heading_title']                  = 'IPG Pay';
-
-// Text
-$_['text_extension']                 = 'Extensions';
-
-// Entry
-$_['entry_app_id']                   = 'App ID';
-
-// Help
-$_['help_ipgpay_setup']                     = '<a target="_blank" href="http://www.opencart.cn/docs/ipgpay">Click here</a> to learn how to set up Ipgpay account.';
-
-// Error
-$_['error_app_id']                   = 'App ID required!';
+/var/www/html/opencart/catalog/language/en-gb/extension/payment/ipgpay.php
